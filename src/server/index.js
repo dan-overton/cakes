214019440a8f7cd697d9a72644ac63ecec47a36f
Daniel Overton <doverton51@gmail.com>
2017-12-12 14:33:59 +0000

    More fun with builds

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const publicweb = process.env.PUBLICWEB || './dist/publicweb';
+const publicweb = './erl/';
 const app = express();
 
 app.use(express.static(publicweb));
